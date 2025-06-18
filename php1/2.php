4671887cf6b08226b346f4f28d1d564bd8ab3c70
Jaqueline <jaqueline.c2004@aluno.ifsc.edu.br>
2025-06-18 08:35:55 -0300

    Organização dos php's

--- php1/2.php
+++ php1/2.php
@@ -1,26 +1,30 @@
-<form action="2.php">
+
+<form >
+<input type="color" name="cor"> 
     <input type="number" name="A">
     <input type="number" name="B">
 
     <input type="submit">
+
 </form>
 
-<?php
-/*programa que imprime a soma de dois numeros informados pelo usuario via get*/
+<?php 
+ /*  programa que imprime a soma de dois números informados pelo usuário via get */
 
-/*verifica se um parametro foi passado*/
+//Verificar se um parametro foi passado
 $A=0; $B=0;
 
-if(isset($_GET["A"])){
-    $A$_GET[A];
+if (isset($_GET["A"])){
+    $A=$_GET["A"];
 }
-if(isset($_GET["B"])){
-    $B$_GET[B];
+if (isset($_GET["B"])){
+    $B=$_GET["B"];
 }
-$A=$_GET["A"];
-$B=$_GET["B"];
 
-echo "a soma de a e b é = "
-echo A."+".B."=".A+B/
+ 
 
-?>
+ echo "A soma de a e b é";
+ echo  $A."+".$B."=".$A+$B;
+
+
+ ?>
--- php1/4.php
+++ php1/4.php
@@ -6,37 +6,33 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Imprima todos os termos de uma PA até N informado pelo usuário</h2>
+        <h2>Imprima todos os temos de uma P.A até N inforado pelo usuário</h2>
 
-    <label for="termoN">Informe o termo</label>
-    <form action="">
-<label for="a1"></label>
-<input id="a1" type="text">
+        <label for="termon">Informe o indice do termo</label>
+        <form action="">
+            <label for="a1"></label>
+            <input id="a1"type="text" name="a1">
 
+            <label for="termn">termo n </label>
+            <input id="termon"type="text" name="n">
 
+            <label for="r" > razao</label>
+            <input id="r"type="text" name="r">
+            
+            <input type="submit">
+        </form>
+        <?php 
 
-    <input id="termoN" type="text" name="N">
-    a1=0
-    r=3
-    </form>
-    <?php
-    $a1=0;
-    $r=3;
-
-    $$N= isset($GET["N"])?$_GET["N"]:1
-    $i=1;
-$termoN=0; //a1
-    for ($i = 1; $i <= $N; $i++) {
-        echo "Termo: " . $termoN;
-        $termoN+=$r;
-      }
-
-   
-
-
-
+        $r= isset($_GET["r"])?$_GET["r"]:1;
+        $n= isset($_GET["n"])?$_GET["n"]:1;
+        $i=1;
+        $termon= isset($_GET["a1"])?$_GET["a1"]:0;
+        for ($i=1;$i<=$n;$i++){
+            echo "Termo:".$termon." - ";
+            $termon+=$r;
+        }
+        ?>
+        
     
-   
-    ?>
 </body>
 </html>
